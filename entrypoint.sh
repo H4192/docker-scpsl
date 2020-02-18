ac057909f8865c685b5b59917c6ab862f17aeb5a
Markus Selin <markus.selin@live.fi>
2020-02-18 17:36:38 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 apt-get -y update
 
+mkdir -p /home/container/steamcmd
+cd /home/container/steamcmd
+wget -qO- 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz' | tar zxf - 
 mkdir -p /home/container/server/
 
 /home/container/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container/server +app_update 996560 -validate +exit 
